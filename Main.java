48767d50a9199f289282e6363c8ce7a4a049f364
Matias RODEGHIERO <mrodeghiero@edenor.com>
2021-04-18 20:17:04 -0300

    Cambio en mensaje

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
 	public static void main(String[] args){
 		System.out.println("Hola mundo! desde java");
-		System.out.println("Agregada la conexión a Github");
+		System.out.println("Agregada la conexión a Github de Matias");
 	}
 }
